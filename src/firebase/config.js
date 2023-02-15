378bd8b99e8e1e5e598de0b35f90cb7771f648fc
johanicp123 <johanicp123@gmail.com>
2023-02-15 03:04:25 -0500

    actualizacion con redux

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/auth';
+import {getFirestore} from 'firebase/firestore/lite';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAIwLiBsMf1oCAtGeFz8xprZOJrLjsd090",
+  authDomain: "react-journal-99478.firebaseapp.com",
+  projectId: "react-journal-99478",
+  storageBucket: "react-journal-99478.appspot.com",
+  messagingSenderId: "731670923401",
+  appId: "1:731670923401:web:d81bad88297e441d4c5e58"
+};
+
+// Initialize Firebase
+export const FirebaseApp = initializeApp(firebaseConfig);
+
+export const FirebaseAuth = getAuth(FirebaseApp);
+
+export const FirebaseDB = getFirestore(FirebaseApp);
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { JorunalApp } from "./JorunalApp";
+import { store } from "./store";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <JorunalApp />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <JorunalApp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
