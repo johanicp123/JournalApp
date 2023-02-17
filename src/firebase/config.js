cdbf9d75fe80b310004859eed229d99eb09123ff
johanicp123 <johanicp123@gmail.com>
2023-02-17 01:58:56 -0500

    actu

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth';
-import {getFirestore} from 'firebase/firestore/lite';
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore/lite";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -13,7 +13,7 @@ const firebaseConfig = {
   projectId: "react-journal-99478",
   storageBucket: "react-journal-99478.appspot.com",
   messagingSenderId: "731670923401",
-  appId: "1:731670923401:web:d81bad88297e441d4c5e58"
+  appId: "1:731670923401:web:d81bad88297e441d4c5e58",
 };
 
 // Initialize Firebase
--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -12,10 +12,20 @@ initialState: {
 },
  reducers: {
     login: (state,action) =>{
-
+        state.status= 'authenticated',
+        state.uid=payload.uid,
+        state.email=payload.email,
+        state.displayName= payload.displayName,
+        state.photoURL= payload.photoURL,
+        state.errorMessage= null;
     },
-    logout: (state,payload) =>{
-
+    logout: (state,{payload}) =>{
+        state.status= 'not-authenticated',
+        state.uid=null,
+        state.email=null,
+        state.displayName= null,
+        state.photoURL= null,
+        state.errorMessage= payload.errorMessage;
     },
     checkingCredentials: (state) =>{
         state.status = 'checking';
